cff764b70635660acc18eb7e6440e5fc6b481744
codeWriter9 <nerdsubhankar2006@gmail.com>
2026-06-17 20:25:44 +0530

    Rename App class to App1

--- src/main/java/kata/java/App1.java
+++ src/main/java/kata/java/App1.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.*;
  */
 @SpringBootApplication
 @Slf4j
-public class App implements CommandLineRunner
+public class App1 implements CommandLineRunner
 {
     public static void main( String[] args )
     {
