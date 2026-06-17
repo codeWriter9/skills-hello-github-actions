6eddf71cdc5f57a531cf21723d1d56b3024a1cde
codeWriter9 <nerdsubhankar2006@gmail.com>
2026-06-17 20:19:35 +0530

    Create App.java for Spring Boot application

---
+++ src/main/java/kata/zero/App.java
@@ -0,0 +1,25 @@
+package kata.zero;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@Slf4j
+public class App implements CommandLineRunner
+{
+    public static void main( String[] args )
+    {
+	    SpringApplication.run(App.class, args);
+    }
+
+    public void run(String [] args) 
+    {	   
+		System.out.println("Hello World! ");
+		log.debug( "Hello World! " );
+    }
+}
