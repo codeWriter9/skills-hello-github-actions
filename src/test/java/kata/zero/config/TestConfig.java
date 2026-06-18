386b8dd2a5b5629a8d01f33483bae4b3cd9361db
codeWriter9 <nerdsubhankar2006@gmail.com>
2026-06-18 21:16:14 +0530

    Add empty TestConfig class for testing purposes

---
+++ src/test/java/kata/zero/config/TestConfig.java
@@ -0,0 +1,12 @@
+package kata.zero.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+/**
+ *
+ *
+ * Empty Test Configuration
+ *
+ */
+@TestConfiguration
+public class TestConfig {}
